be44b17e6228816b660a163f15ebd472ebb5c02f
S3B4S5 <sebastian75601012@outlook.com>
2025-04-14 01:09:02 -0400

    🔨 Arreglando la BD

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,7 @@
 # App: orders/models.py
 from django.db import models
 import uuid
+from datetime import timedelta
 
 class PaymentDetail(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -13,7 +14,7 @@ class ShippingMethod(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
-    estimated_days = models.IntegerField()
+    estimated_time = models.DurationField(default=timedelta(0))
 
 class OrderStatus(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -27,7 +28,6 @@ class Order(models.Model):
     status = models.ForeignKey(OrderStatus, on_delete=models.CASCADE)
     date = models.DateField()
     time = models.TimeField()
-    state = models.CharField(max_length=50)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField()
     modified_at = models.DateTimeField()
@@ -46,8 +46,8 @@ class OrderItem(models.Model):
 class Invoice(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    nit = models.CharField(max_length=50, default='0')
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    issue_date = models.DateTimeField()
-    status = models.CharField(max_length=50)
+    razon_social = models.CharField(max_length=50, default='Ninguna')
     created_at = models.DateTimeField()
     modified_at = models.DateTimeField()
