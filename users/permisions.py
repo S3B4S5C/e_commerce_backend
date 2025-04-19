c8c2b1e309087011e4d27164b3a1475bd2e44b6e
S3B4S5 <sebastian75601012@outlook.com>
2025-04-19 02:56:23 -0400

    🔑 Admin permissions

---
+++ users/permisions.py
@@ -0,0 +1,15 @@
+# permissions.py
+from rest_framework.permissions import BasePermission
+
+class IsAdminRole(BasePermission):
+    """
+    Permite el acceso solo a usuarios con rol 'ADMIN'.
+    """
+
+    def has_permission(self, request, view):
+        print("Checking permission for user:", request.user)
+        return bool(
+            request.user and 
+            request.user.is_authenticated and 
+            getattr(request.user, 'role', None) == 'ADMIN'
+        )
