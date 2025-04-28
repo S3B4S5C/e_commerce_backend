7ba1134b54266105a223879c5249f525cf92f6b9
Joel Cuéllar <joelcuellar2311@gmail.com>
2025-04-28 12:44:56 -0400

    arreglo

--- assistant/urls.py
+++ assistant/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import VoiceAssistantView
 
 urlpatterns = [
-    path("voz/", VoiceAssistantView.as_view(), name="asistente-voz"),  # ← este `.as_view()` es lo importante
+    path("voz/", VoiceAssistantView.as_view(), name="asistente-voz")
 ]
